1cb16f89cfe09ad7f9edadbe6a518a932323c7e9
Cath <61656259+KirashuYakima@users.noreply.github.com>
2023-11-04 20:56:53 +0000

    link embed push

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,8 @@
 export default defineAppConfig({
   docus: {
     title: 'ChocoGuard',
-    description: 'Documentation Discord Bot.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    description: 'We have been sailing in the protection of the users on Discord since 2023!',
+    image: 'https://media.discordapp.net/attachments/573508617873915914/1120472706412974131/image.png?width=872&height=314',
   },
     //socials: {
       //twitter: 'nuxt_js',
